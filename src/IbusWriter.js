23c21e23b0022de02971b5d2c731652bd9448d65
Constantin-Cosmin Craciun <cwave@cwave-mbp.cwavesoftware.com>
2022-03-29 09:11:13 +0300

    Introducing IbusWriter

---
+++ src/IbusWriter.js
@@ -0,0 +1,29 @@
+var IbusInterface = require('./IbusInterface.js');
+var IbusDevices = require('./IbusDevices.js');
+
+var device = process.argv[2];
+
+// data
+var ibusInterface = new IbusInterface(device);
+
+function init() {
+    ibusInterface.startup();
+}
+
+process.on('SIGINT', onSignalInt);
+
+// implementation
+function onSignalInt() {
+    ibusInterface.shutdown(function() {
+        process.exit();
+    });
+}
+// main start
+init();
+
+packet = Buffer.from(process.argv[3], 'hex');
+ibusInterface.sendMessage({
+    src: packet[0],
+    dst: packet[2],
+    msg: packet.slice(3)
+});
